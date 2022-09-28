aeca7d985c6ead2c0335d82743e40cfdce4d88bc
Капешко Олег А <oleg.kapeshko@belwest.com>
2022-09-28 09:59:17 +0300

    0.3.1 - minor fixes

--- src/Process.php
+++ src/Process.php
@@ -66,81 +66,64 @@ class Process extends \CommonDBTM
      * @return $item
     **/
     static function modifyNotification($item) {
-        // add to notification's template tags of assigned user's photo
+        
         $assigned = $item->data['##ticket.assigntousers##'];
         $item->data['##ticket.assigntousers##'] = explode(',', $assigned)[0];
         $tickets_id = (int)$item->data['##ticket.id##'];
         $tu = new \Ticket_User();
         $assign = current($tu->find(['tickets_id' => $tickets_id, 'type' => 2], ['id'], 1));
         $requester = current($tu->find(['tickets_id' => $tickets_id, 'type' => 1], ['id'], 1));
-        $p = new self;
-        $proc = current($p->find(['users_id' => $assign['users_id']], [], 1));
-        $item->data['##ticket.assigntouserphoto.small##'] = '<img height="48" width="48" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
-        $item->tag_descriptions['tag']['##ticket.assigntouserphoto.small##'] = [
-            'tag'       => 'ticket.assigntouserphoto.small',
-            'value'     => 1,
-            'label'     => __('Фото специалиста', 'etn'),
-            'events'    => 0,
-            'lang'      => 1
-        ];
-        $item->data['##ticket.assigntouserphoto.medium##'] = '<img height="96" width="96" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
-        $item->tag_descriptions['tag']['##ticket.assigntouserphoto.medium##'] = [
-            'tag'       => 'ticket.assigntouserphoto.medium',
-            'value'     => 1,
-            'label'     => __('Фото специалиста', 'etn'),
-            'events'    => 0,
-            'lang'      => 1
-        ];
-        $item->data['##ticket.assigntouserphoto.large##'] = '<img height="144" width="144" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
-        $item->tag_descriptions['tag']['##ticket.assigntouserphoto.large##'] = [
-            'tag'       => 'ticket.assigntouserphoto.large',
-            'value'     => 1,
-            'label'     => __('Фото специалиста', 'etn'),
-            'events'    => 0,
-            'lang'      => 1
-        ];
+        if($assign) {
+            // add to notification's template tags of assigned user's photo
+            $p = new self;
+            $proc = current($p->find(['users_id' => $assign['users_id']], [], 1));
+            $item->data['##ticket.assigntouserphoto.small##'] = '<img height="48" width="48" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
+            $item->tag_descriptions['tag']['##ticket.assigntouserphoto.small##'] = [
+                'tag'       => 'ticket.assigntouserphoto.small',
+                'value'     => 1,
+                'label'     => __('Фото специалиста', 'etn'),
+                'events'    => 0,
+                'lang'      => 1
+            ];
+            $item->data['##ticket.assigntouserphoto.medium##'] = '<img height="96" width="96" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
+            $item->tag_descriptions['tag']['##ticket.assigntouserphoto.medium##'] = [
+                'tag'       => 'ticket.assigntouserphoto.medium',
+                'value'     => 1,
+                'label'     => __('Фото специалиста', 'etn'),
+                'events'    => 0,
+                'lang'      => 1
+            ];
+            $item->data['##ticket.assigntouserphoto.large##'] = '<img height="144" width="144" src="'.$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/front/document.send.php?docid='.$proc['documents_id'].'" />';
+            $item->tag_descriptions['tag']['##ticket.assigntouserphoto.large##'] = [
+                'tag'       => 'ticket.assigntouserphoto.large',
+                'value'     => 1,
+                'label'     => __('Фото специалиста', 'etn'),
+                'events'    => 0,
+                'lang'      => 1
+            ];
 
-        // add to notification's template tag of assigned user's title
-        $u = new \User();
-        $user = current($u->find(['id' => $assign['users_id']], [], 1));
-        $t = new \UserTitle();
-        $title = current($t->find(['id' => $user['usertitles_id']], [], 1));
-        $item->data['##ticket.assigntousertitle##'] = $title['name'];
-        $item->tag_descriptions['tag']['##ticket.assigntousertitle##'] = [
-            'tag'       => 'ticket.assigntousertitle',
-            'value'     => 1,
-            'label'     => __('Должность специалиста', 'etn'),
-            'events'    => 0,
-            'lang'      => 1
-        ];
+            // add to notification's template tag of assigned user's title
+            $u = new \User();
+            $user = current($u->find(['id' => $assign['users_id']], [], 1));
+            $t = new \UserTitle();
+            $title = current($t->find(['id' => $user['usertitles_id']], [], 1));
+            $item->data['##ticket.assigntousertitle##'] = $title['name'];
+            $item->tag_descriptions['tag']['##ticket.assigntousertitle##'] = [
+                'tag'       => 'ticket.assigntousertitle',
+                'value'     => 1,
+                'label'     => __('Должность специалиста', 'etn'),
+                'events'    => 0,
+                'lang'      => 1
+            ];
+        } else {
+            $item->data['##ticket.assigntouserphoto.small##'] = '';
+            $item->data['##ticket.assigntouserphoto.medium##'] = '';
+            $item->data['##ticket.assigntouserphoto.large##'] = '';
+            $item->data['##ticket.assigntousertitle##'] = '';
+        }
 
         // add to notification's template tags of solved ticket's ratings
-        if($item->raiseevent == 'solved') {
-            $ts = new \TicketSatisfaction();
-            if($ticket = current($ts->find(['tickets_id' => $tickets_id], [], 1))) {
-                $ts->getFromDB($ticket['id']);
-                $ts->fields['tickets_id'] = $tickets_id;
-                $ts->fields['date_begin'] = date('Y-m-d H:i:s');
-                $ts->fields['satisfaction'] = isset($ts->fields['satisfaction']) ? $ts->fields['satisfaction'] : 5;
-                $ts->updateInDB(array_keys($ts->fields));
-            } else {
-                $input['date_begin'] = date('Y-m-d H:i:s');
-                $input['satisfaction'] = 5;
-                $input['tickets_id'] = $tickets_id;
-                $ts->add($input);
-            }
-            $r = new Rating();
-            if($rating = current($r->find(['tickets_id' => $tickets_id], [], 1))) {
-                $r->getFromDB($rating['id']);
-                if($rating['status']) $r->fields['status'] = 0;
-                $r->fields['date_create'] = date('Y-m-d H:i:s');
-                $r->updateInDB(array_keys($r->fields));
-            } else {
-                $input['tickets_id'] = $tickets_id;
-                $input['date_create'] = date('Y-m-d H:i:s');
-                $r->add($input);
-            }
-            $style = '
+        $style = '
                 display:inline-block;
                 background:#3200F0;
                 color:#ffffff;
@@ -155,48 +138,74 @@ class Process extends \CommonDBTM
                 mso-padding-alt:0px;
                 border-radius:3px;
             ';
-            $url = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/plugins/etn/front/status.php?'.'users_id='.$requester['users_id'].'&tickets_id='.$tickets_id;
-            $item->data['##ticket.rating.1##'] = '<a href="'.$url.'&rating=1" style="'.$style.'">1</a>';
-            $item->tag_descriptions['tag']['##ticket.rating.1##'] = [
-                'tag'       => 'ticket.rating.1',
-                'value'     => 1,
-                'label'     => __('Оценка 1', 'etn'),
-                'events'    => 0,
-                'lang'      => 1
-            ];
-            $item->data['##ticket.rating.2##'] = '<a href="'.$url.'&rating=2" style="'.$style.'">2</a>';
-            $item->tag_descriptions['tag']['##ticket.rating.2##'] = [
-                'tag'       => 'ticket.rating.2',
-                'value'     => 1,
-                'label'     => __('Оценка 2', 'etn'),
-                'events'    => 0,
-                'lang'      => 1
-            ];
-            $item->data['##ticket.rating.3##'] = '<a href="'.$url.'&rating=3" style="'.$style.'">3</a>';
-            $item->tag_descriptions['tag']['##ticket.rating.3##'] = [
-                'tag'       => 'ticket.rating.3',
-                'value'     => 1,
-                'label'     => __('Оценка 3', 'etn'),
-                'events'    => 0,
-                'lang'      => 1
-            ];
-            $item->data['##ticket.rating.4##'] = '<a href="'.$url.'&rating=4" style="'.$style.'">4</a>';
-            $item->tag_descriptions['tag']['##ticket.rating.4##'] = [
-                'tag'       => 'ticket.rating.4',
-                'value'     => 1,
-                'label'     => __('Оценка 4', 'etn'),
-                'events'    => 0,
-                'lang'      => 1
-            ];
-            $item->data['##ticket.rating.5##'] = '<a href="'.$url.'&rating=5" style="'.$style.'">5</a>';
-            $item->tag_descriptions['tag']['##ticket.rating.5##'] = [
-                'tag'       => 'ticket.rating.5',
-                'value'     => 1,
-                'label'     => __('Оценка 5', 'etn'),
-                'events'    => 0,
-                'lang'      => 1
-            ];
+        $url = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/plugins/etn/front/status.php?'.'users_id='.$requester['users_id'].'&tickets_id='.$tickets_id;
+        //if($item->raiseevent == 'solved') {
+        $ts = new \TicketSatisfaction();
+        if($ticket = current($ts->find(['tickets_id' => $tickets_id], [], 1))) {
+            $ts->getFromDB($ticket['id']);
+            $ts->fields['tickets_id'] = $ticket['id'];
+            $ts->fields['tickets_id'] = $tickets_id;
+            $ts->fields['date_begin'] = date('Y-m-d H:i:s');
+            $ts->fields['satisfaction'] = isset($ts->fields['satisfaction']) ? $ts->fields['satisfaction'] : 5;
+            $ts->updateInDB(array_keys($ts->fields));
+        } else {
+            $input['date_begin'] = date('Y-m-d H:i:s');
+            $input['satisfaction'] = 5;
+            $input['tickets_id'] = $tickets_id;
+            $ts->add($input);
         }
+        $r = new Rating();
+        if($rating = current($r->find(['tickets_id' => $tickets_id], [], 1))) {
+            $r->getFromDB($rating['id']);
+            if($rating['status']) $r->fields['status'] = 0;
+            $r->fields['date_create'] = date('Y-m-d H:i:s');
+            $r->updateInDB(array_keys($r->fields));
+        } else {
+            $input['tickets_id'] = $tickets_id;
+            $input['date_create'] = date('Y-m-d H:i:s');
+            $r->add($input);
+        }
+        $item->data['##ticket.rating.1##'] = '<a href="'.$url.'&rating=1" style="'.$style.'">1</a>';
+        $item->tag_descriptions['tag']['##ticket.rating.1##'] = [
+            'tag'       => 'ticket.rating.1',
+            'value'     => 1,
+            'label'     => __('Оценка 1', 'etn'),
+            'events'    => 0,
+            'lang'      => 1
+        ];
+        $item->data['##ticket.rating.2##'] = '<a href="'.$url.'&rating=2" style="'.$style.'">2</a>';
+        $item->tag_descriptions['tag']['##ticket.rating.2##'] = [
+            'tag'       => 'ticket.rating.2',
+            'value'     => 1,
+            'label'     => __('Оценка 2', 'etn'),
+            'events'    => 0,
+            'lang'      => 1
+        ];
+        $item->data['##ticket.rating.3##'] = '<a href="'.$url.'&rating=3" style="'.$style.'">3</a>';
+        $item->tag_descriptions['tag']['##ticket.rating.3##'] = [
+            'tag'       => 'ticket.rating.3',
+            'value'     => 1,
+            'label'     => __('Оценка 3', 'etn'),
+            'events'    => 0,
+            'lang'      => 1
+        ];
+        $item->data['##ticket.rating.4##'] = '<a href="'.$url.'&rating=4" style="'.$style.'">4</a>';
+        $item->tag_descriptions['tag']['##ticket.rating.4##'] = [
+            'tag'       => 'ticket.rating.4',
+            'value'     => 1,
+            'label'     => __('Оценка 4', 'etn'),
+            'events'    => 0,
+            'lang'      => 1
+        ];
+        $item->data['##ticket.rating.5##'] = '<a href="'.$url.'&rating=5" style="'.$style.'">5</a>';
+        $item->tag_descriptions['tag']['##ticket.rating.5##'] = [
+            'tag'       => 'ticket.rating.5',
+            'value'     => 1,
+            'label'     => __('Оценка 5', 'etn'),
+            'events'    => 0,
+            'lang'      => 1
+        ];
+        //}
 
         // add to notification's template tag of ticket's priority up
         $item->data['##ticket.priorityup##'] = '<a href="'.$url.'&priority_up=4" style="'.$style.'">'.__('Повысить приоритет', 'etn').'</a>';
